66d316d76457d9c09b47511d093e691143ac7906
PCLE <vjyasona@gmail.com>
2025-02-19 14:40:21 +0800

    n

--- Login.php
+++ Login.php
@@ -27,7 +27,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel ="stylesheet" href ="css/Register.css">
-    <title>Register</title>
+    <title>PCLE</title>
 </head>
 <body>
     <form action ="Login.php" method ="POST"   >
